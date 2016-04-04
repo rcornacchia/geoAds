d56c04a6f7d112660713c7485ca17d8b0a9a6280
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-04 17:27:50 -0400

    added form

--- tiles/index.js
+++ tiles/index.js
@@ -103,3 +103,21 @@ function initMap() {
         gMarkers = [];
     }
 }
+
+$('#InfroTextSubmit').click(function(){
+
+    if ($('#title').val()==="") {
+      // invalid
+      $('#title').next('.help-inline').show();
+      return false;
+    }
+    else {
+      // submit the form here
+      // $('#InfroText').submit();
+
+      return true;
+    }
+
+
+
+});
