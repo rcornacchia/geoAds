be4d80912d7bdbd1583eebd49046cdfe9ffb599f
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-04 17:31:46 -0400

    fixed index.js

--- tiles/index.js
+++ tiles/index.js
@@ -103,21 +103,3 @@ function initMap() {
         gMarkers = [];
     }
 }
-
-$('#InfroTextSubmit').click(function(){
-
-    if ($('#title').val()==="") {
-      // invalid
-      $('#title').next('.help-inline').show();
-      return false;
-    }
-    else {
-      // submit the form here
-      // $('#InfroText').submit();
-
-      return true;
-    }
-
-
-
-});
