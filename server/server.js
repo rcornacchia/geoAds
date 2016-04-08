ff1fdd00728ae2eb439a4004289bbdff32774c1b
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-04-08 15:50:01 -0400

    Streaming server now pushes JSON location to SQS

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
-var express = require('express'),
-    swig = require('swig'),
-    path = require('path'),
-    morgan = require('morgan'),
-    app = express();
+var app = require('express')();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
+/*
+It is assumed that credentials are set in ~/.aws/credentials or
+AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY have been exported
+as environment variables.
+*/
+var AWS = require('aws-sdk');
+var SQS = new AWS.SQS({
+    region: 'us-east-1'
+});
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, 'views'));
+// Configure app for listening for post requests
+app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-
 app.listen(8081)
 
-app.get('/', function(req, res) {
-    res.render('index');
+app.post('/location', function(req, res) {
+
+    // We might want to do some input checking / validation here
+
+    var params = {
+        MessageBody: JSON.stringify(req.body),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/746215537304/geoAds',
+    };
+
+    SQS.sendMessage(params, function(error, data) {
+        if (error) console.log(error, error.stack);
+        else console.log(data);
+    });
+
+    res.json(req.body);
 });
