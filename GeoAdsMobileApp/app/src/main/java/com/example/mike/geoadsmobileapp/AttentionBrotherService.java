9c95c7f5792fc57c2d06408840bf295b9eb17e9f
mbl2109 <mbl2109@columbia.edu>
2016-04-22 16:26:54 -0400

    Android App: app recognizes when screen turned on/off

--- GeoAdsMobileApp/app/src/main/java/com/example/mike/geoadsmobileapp/AttentionBrotherService.java
+++ GeoAdsMobileApp/app/src/main/java/com/example/mike/geoadsmobileapp/AttentionBrotherService.java
@@ -1,7 +1,10 @@
 package com.example.mike.geoadsmobileapp;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 
 public class AttentionBrotherService extends Service {
@@ -15,8 +18,49 @@ public class AttentionBrotherService extends Service {
        * Use a broadcast receiver: http://developer.android.com/reference/android/content/BroadcastReceiver.html
      */
 
+    /*
+        For screen lock broadcast receiver I followed this tutorial:
+        https://thinkandroid.wordpress.com/2010/01/24/handling-screen-off-and-screen-on-intents/
+     */
+    private final BroadcastReceiver screenOnOffReceiver = new BroadcastReceiver() {
+        boolean screenOn = true;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                screenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                screenOn = false;
+            }
+            Intent i = new Intent(context, AttentionBrotherService.class);
+            i.putExtra("screenState", screenOn);
+            context.startService(i);
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenOnOffReceiver, filter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        boolean screenOn = intent.getBooleanExtra("screenState", true);
+        if(screenOn) {
+            System.out.println("AttentionBrotherService received screenOn = true");
+        }
+        else {
+            System.out.println("AttentionBrotherService received screenOff = false");
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
+
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
